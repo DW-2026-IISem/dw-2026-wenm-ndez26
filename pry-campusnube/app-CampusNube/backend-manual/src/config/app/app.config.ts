84a19c017f2e383086822c75720a0a58dc0098f7
Wenyure Méndez <wenyu3@hotmail.com>
2026-09-10 19:52:36 -0500

    feat: add app.config.ts

---
+++ pry-campusnube/app-CampusNube/backend-manual/src/config/app/app.config.ts
@@ -0,0 +1,8 @@
+import { registerAs } from '@nestjs/config';
+import { APP_CONFIG_NAME, APP_DEFAULTS } from './app.constants.js';
+import { Environment } from '../environment/env.interface.js';
+
+export const appConfig = registerAs(APP_CONFIG_NAME, () => ({
+  port: parseInt(process.env.PORT || String(APP_DEFAULTS.PORT), 10),
+  nodeEnv: (process.env.NODE_ENV as Environment) || APP_DEFAULTS.NODE_ENV,
+}));
