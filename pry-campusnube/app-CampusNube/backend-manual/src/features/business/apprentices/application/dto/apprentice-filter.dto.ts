49c13f53dbce691829e1fb981b62f2a793c158df
Wenyure Méndez <wenyu3@hotmail.com>
2026-09-12 00:32:04 -0500

    feat: add dto apprentice-filter.dto.ts

---
+++ pry-campusnube/app-CampusNube/backend-manual/src/features/business/apprentices/application/dto/apprentice-filter.dto.ts
@@ -0,0 +1,30 @@
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import {
+  IsInt,
+  IsOptional,
+  IsPositive,
+  IsString,
+  Min,
+} from 'class-validator';
+
+export class ApprenticeFilterDto {
+  @ApiPropertyOptional({ example: 1, default: 1 })
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  page?: number;
+
+  @ApiPropertyOptional({ example: 10, default: 10 })
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @IsPositive()
+  limit?: number;
+
+  @ApiPropertyOptional({ example: 'Juan' })
+  @IsOptional()
+  @IsString()
+  name?: string;
+}
