1295beba6231d95b3dd6f390c5f3e8ec101236f0
Wenyure Méndez <wenyu3@hotmail.com>
2026-09-12 11:50:53 -0500

    test: verify learning content flow

--- pry-campusnube/app-CampusNube/backend-manual/src/features/business/learning-content/domain/exceptions/invalid-lesson-order.exception.ts
+++ pry-campusnube/app-CampusNube/backend-manual/src/features/business/learning-content/domain/exceptions/invalid-lesson-order.exception.ts
@@ -1,6 +1,6 @@
-import { DomainException } from '../../../../../common/exceptions/domain.exception.js';
+import { BadRequestException } from '../../../../../common/exceptions/bad-request.exception.js';
 
-export class InvalidLessonOrderException extends DomainException {
+export class InvalidLessonOrderException extends BadRequestException {
   constructor() {
     super('El orden de la lección debe ser un entero mayor a 0');
   }
